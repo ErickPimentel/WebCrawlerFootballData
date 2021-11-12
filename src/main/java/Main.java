58e4909ccc8d1de4be31d437ab02a58d7cf5352e
Pimentel <EPIMENT5@ford.com>
2021-11-11 21:54:54 -0300

    Funcionalidade de buscar os links dos paises primearios e secundarios melhorada e funcionalidade de pegar os csvs de cada pais primarios e secundarios melhorada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         WebCrawler wb = new WebCrawler();
 
-        List<String> lista_link_paises = wb.busca_link_paises();
+        wb.busca_link_paises();
+        wb.run();
 
-        List<Arquivo> lista_arquivos = wb.run(lista_link_paises);
-//
 //        GerenciadorCSV g = new GerenciadorCSV();
 //        g.baixa(lista_arquivos);
+
 //        g.adicionaColunas(lista_arquivos);
 
 
--- src/main/java/WebCrawler.java
+++ src/main/java/WebCrawler.java
@@ -9,7 +9,13 @@ import java.util.List;
 
 public class WebCrawler {
 
-    public List<String> busca_link_paises() throws IOException {
+    private List<String> lista_link_paises_principais = new ArrayList<String>();
+    private List<String> lista_link_paises_secundarios = new ArrayList<String>();
+
+    private List<Arquivo> lista_arquivos_paises_principais = new ArrayList<Arquivo>();
+    private List<Arquivo> lista_arquivos_paises_secundarios = new ArrayList<Arquivo>();
+
+    public void busca_link_paises() throws IOException {
 
         String url = "https://www.football-data.co.uk/";
 
@@ -17,85 +23,75 @@ public class WebCrawler {
 
         Elements elementos_paises = document.select("body > table > tbody > tr > td > table > tbody > tr > td > div > a");
 
-        List<String> lista_link_paises = new ArrayList<String>();
         String link_pais;
         int count = 0;
-        for (Element pais: elementos_paises) {
-            if(count > 37 && count < 60){
+        for (Element pais : elementos_paises) {
+            if (count > 37 && count < 60) {
                 link_pais = pais.attr("abs:href");
-                if (!lista_link_paises.contains(link_pais)){
-                    lista_link_paises.add(link_pais);
+                if (!lista_link_paises_principais.contains(link_pais)) {
+                    lista_link_paises_principais.add(link_pais);
                 }
             }
 
-            if(count > 62 && count < 94){
+            if (count > 62 && count < 94) {
                 link_pais = pais.attr("abs:href");
-                if (!lista_link_paises.contains(link_pais)){
-                    lista_link_paises.add(link_pais);
+                if (!lista_link_paises_secundarios.contains(link_pais)) {
+                    lista_link_paises_secundarios.add(link_pais);
                 }
             }
             count++;
         }
-
-        return lista_link_paises;
 
     }
 
-    public List<Arquivo> run(List<String> lista_link_paises) throws IOException {
+    public void run() throws IOException {
 
-        List<Arquivo> lista_arquivos = new ArrayList<Arquivo>();
-        int count = 0;
-        for (String link_pais: lista_link_paises) {
-            if (count <= 12){
+        for (String link_pais : lista_link_paises_principais) {
+            String url = link_pais;
+            Document document = Jsoup.connect(url).get();
 
-                String url = link_pais;
-                Document document = Jsoup.connect(url).get();
+            Elements links = document.select("a[href^=mmz]");
 
-                Elements links = document.select("a[href^=mmz]");
-
-                Elements b_elements = document.select("b");
-                String country = null;
-                for (Element campo_country: b_elements) {
-                    if (campo_country.text().contains("Data Files")){
-                        country = campo_country.text().substring(12);
-                    }
-                }
-
-                String link_csv;
-                String league;
-                String season;
-
-                for (Element link: links) {
-
-                    link_csv = link.attr("abs:href");
-                    league = link.text();
-                    season = link.attr("href").substring(8,12);
-
-                    Arquivo d = new Arquivo(link_csv, country, league, season);
-                    lista_arquivos.add(d);
-                }
-
-            }else {
-
-                String url = link_pais;
-                Document document = Jsoup.connect(url).get();
-
-                String link_csv;
-                Elements links = document.select("a[href$=.csv]");
-                for (Element link: links) {
-                    if (link.text().contains("CSV")){
-                        link_csv = link.attr("abs:href");
-                        Arquivo d = new Arquivo(link_csv);
-                        lista_arquivos.add(d);
-                    }
+            Elements b_elements = document.select("b");
+            String country = null;
+            for (Element campo_country : b_elements) {
+                if (campo_country.text().contains("Data Files")) {
+                    country = campo_country.text().substring(12);
                 }
             }
 
+            String link_csv;
+            String league;
+            String season;
 
-            count++;
+            for (Element link : links) {
+
+                link_csv = link.attr("abs:href");
+                league = link.text();
+                season = link.attr("href").substring(8, 12);
+
+                Arquivo d = new Arquivo(link_csv, country, league, season);
+                lista_arquivos_paises_principais.add(d);
+            }
         }
 
-        System.out.println(lista_arquivos);
-        return lista_arquivos;
+        for (String link_pais : lista_link_paises_secundarios) {
+            String url = link_pais;
+            Document document = Jsoup.connect(url).get();
+
+            String link_csv;
+            Elements links = document.select("a[href$=.csv]");
+            for (Element link : links) {
+                if (link.text().contains("CSV")) {
+                    link_csv = link.attr("abs:href");
+                    Arquivo d = new Arquivo(link_csv);
+                    lista_arquivos_paises_secundarios.add(d);
+                }
+            }
+
+        }
+    System.out.println(lista_arquivos_paises_principais.toString());
+    System.out.println(lista_arquivos_paises_secundarios.toString());
+
     }
 }
