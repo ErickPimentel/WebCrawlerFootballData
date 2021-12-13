65f55dbee31da98657a5786b6fea34b4a3e8c537
Pimentel <EPIMENT5@ford.com>
2021-12-13 19:10:41 -0300

    Json objects to MongoDB implemented

--- src/main/java/Constantes/ConstantesGerenciadorArquivo.java
+++ src/main/java/Constantes/ConstantesGerenciadorArquivo.java
@@ -12,10 +12,12 @@ public interface ConstantesGerenciadorCSV {
     String COLUNAS_PAISES_SECUNDARIOS = "Country,League," + INICIO_SEASON + V + FIM_SEASON + ",Date,Time,Home,Away,HG,AG,Res,PH,PD,PA,MaxH,MaxD,MaxA,AvgH,AvgD,AvgA" + LINESEP;
     String UNDERSCORE = "_";
     String FORMATO_CSV = ".csv";
+    String FORMATO_JSON = ".json";
     String FILE_PATH_PASTA_V1 = "C:\\Users\\EPIMENT5\\Desktop\\Arquivos\\v1\\";
     String FILE_PATH_PASTA_V2 = "C:\\Users\\EPIMENT5\\Desktop\\Arquivos\\v2\\";
     String FILE_PATH_PASTA_V3 = "C:\\Users\\EPIMENT5\\Desktop\\Arquivos\\v3\\";
     String PAISES_SECUNDARIOS = "PaisesSecundarios";
+    String PAISES_PRINCIPAIS = "PaisesPrincipais";
 
 
 }
